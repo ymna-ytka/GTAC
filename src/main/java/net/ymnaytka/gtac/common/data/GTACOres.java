80c75d59c33e50f0da7539674c3e8fc1073f0e51
ymna-ytka <rogozinskiyroza059@gmail.com>
2025-05-21 19:35:41 +0300

    mini change

--- src/main/java/net/ymnaytka/gtac/common/data/GTACOres.java
+++ src/main/java/net/ymnaytka/gtac/common/data/GTACOres.java
@@ -2,12 +2,7 @@ package net.ymnaytka.gtac.common.data;
 
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.data.worldgen.GTOreDefinition;
-import com.gregtechceu.gtceu.api.data.worldgen.WorldGenLayers;
-import com.gregtechceu.gtceu.api.data.worldgen.generator.veins.DikeVeinGenerator;
-import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gregtechceu.gtceu.common.data.GTOres;
-import net.minecraft.tags.BiomeTags;
-import net.minecraft.util.valueproviders.UniformInt;
 
 import java.util.function.Consumer;
 
@@ -16,8 +11,6 @@ public class GTACOres {
     public static void init() {}
 
     private static GTOreDefinition create(String name, Consumer<GTOreDefinition> config) {
-
         return GTOres.create(GTCEu.id(name), config);
-
     }
 }
--- src/main/java/net/ymnaytka/gtac/data/lang/LangHandler.java
+++ src/main/java/net/ymnaytka/gtac/data/lang/LangHandler.java
@@ -4,5 +4,55 @@ import com.tterrag.registrate.providers.RegistrateLangProvider;
 
 public class LangHandler extends com.gregtechceu.gtceu.data.lang.LangHandler {
 
-    public static void init(RegistrateLangProvider provider) {}
+    public static void init(RegistrateLangProvider provider) {
+        replace(provider, "gtceu.machine.steam_fluid_hatch_notice",
+                "This hatch is for Fluid ingredients! Not to power with steam!");
+        replace(provider, "block.gtceu.steam_fluid_output_hatch", "Bronze Output Hatch");
+        replace(provider, "block.gtceu.steam_fluid_input_hatch", "Bronze Input Hatch");
+        replace(provider, "block.gtceu.lv_air_cooler", "Basic Air Cooler §r");
+        replace(provider, "block.gtceu.mv_air_cooler", "§bAdvanced Air Cooler §r");
+        replace(provider, "block.gtceu.hv_air_cooler", "§6Advanced Air Cooler II§r");
+        replace(provider, "block.gtceu.ev_air_cooler", "§5Advanced Air Cooler III§r");
+        replace(provider, "block.gtceu.iv_air_cooler", "§dElite Air Cooler §r");
+        replace(provider, "block.gtceu.luv_air_cooler", "§dElite Air Cooler II§r");
+        replace(provider, "block.gtceu.zpm_air_cooler", "§cElite Air Cooler III§r");
+        replace(provider, "block.gtceu.lv_cooler", "Basic Cooler §r");
+        replace(provider, "block.gtceu.mv_cooler", "§bAdvanced Cooler §r");
+        replace(provider, "block.gtceu.hv_cooler", "§6Advanced Cooler II§r");
+        replace(provider, "block.gtceu.ev_cooler", "§6Advanced Cooler III§r");
+        replace(provider, "block.gtceu.iv_cooler", "§cElite Cooler §r");
+        replace(provider, "block.gtceu.luv_cooler", "§cElite Cooler II§r");
+        replace(provider, "block.gtceu.zpm_cooler", "§cElite Cooler III§r");
+        replace(provider, "block.gtceu.lv_farmer", "Basic Farmer §r");
+        replace(provider, "block.gtceu.mv_farmer", "§bAdvanced Farmer §r");
+        replace(provider, "block.gtceu.hv_farmer", "§6Advanced Farmer II§r");
+        replace(provider, "block.gtceu.ev_farmer", "§6Advanced Farmer III§r");
+        replace(provider, "block.gtceu.iv_farmer", "§cElite Farmer §r");
+        replace(provider, "block.gtceu.luv_farmer", "§cElite Farmer II§r");
+        replace(provider, "block.gtceu.zpm_farmer", "§cElite Farmer III§r");
+        replace(provider, "block.gtceu.lv_twilight_farmer", "Basic Twilight Farmer §r");
+        replace(provider, "block.gtceu.mv_twilight_farmer", "§bAdvanced Twilight Farmer §r");
+        replace(provider, "block.gtceu.hv_twilight_farmer", "§6Advanced Twilight Farmer II§r");
+        replace(provider, "block.gtceu.ev_twilight_farmer", "§6Advanced Twilight Farmer III§r");
+        replace(provider, "block.gtceu.iv_twilight_farmer", "§cElite Twilight Farmer §r");
+        replace(provider, "block.gtceu.luv_twilight_farmer", "§cElite Twilight Farmer II§r");
+        replace(provider, "block.gtceu.zpm_twilight_farmer", "§cElite Twilight Farmer III§r");
+        replace(provider, "block.gtceu.lp_steam_autoclave", "Low Pressure Steam Autoclave");
+        replace(provider, "block.gtceu.lp_steam_mixer", "Low Pressure Steam Mixer");
+        replace(provider, "block.gtceu.hp_steam_autoclave", "High Pressure Steam Autoclave");
+        replace(provider, "block.gtceu.hp_steam_mixer", "High Pressure Steam Mixer");
+
+        provider.add("gtceu.air_cooler", "Air Cooler");
+        provider.add("gtceu.cooler", "Cooling");
+        provider.add("gtceu.twilight_farmer", "Twilight Farmer");
+        provider.add("gtceu.farmer", "Farmer");
+        provider.add("gtceu.bronze_mixer", "Bronze Mixer");
+        provider.add("gtceu.steam_blast_furnace", "Steam Blast Furnace");
+        provider.add("gtceu.steam_coke_oven", "Steam Coke Oven");
+        provider.add("gtceu.steam_magic_bath", "Steam Magic Bath");
+        provider.add("gtceu.tar_smokehouse", "Smoking");
+        provider.add("gtceu.induction_furnace", "Induction Furnace");
+        provider.add("gtceu.natural_cleaner", "Natural Cleaner");
+        provider.add("gtceu.fertilizer_buck", "Fertilizer Buck");
+    }
 }
